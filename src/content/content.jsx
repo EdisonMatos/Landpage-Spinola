10b5ae3ca81e5fb6d14fb47dd06a29333c765b29
Gabriel <gabrielsouza65115@gmail.com>
2024-09-25 16:00:25 -0300

    fix: Converte as imagens da About em webp

--- src/content/content.jsx
+++ src/content/content.jsx
@@ -8,7 +8,7 @@ import bgHeroImg from "../assets/imgs/hero/bgHero.jpg";
 import imgFeatures from "../assets/imgs/features/imgServices.webp";
 import imgDivisor from "../assets/imgs/features/imgDivisor.jpg";
 
-import aboutImg1 from "../assets/imgs/about/aboutImg.jpg";
+import aboutImg1 from "../assets/imgs/about/aboutImg.webp";
 import aboutSocialImg from "../assets/imgs/about/aboutSocial.png";
 
 import imgSteps from "../assets/imgs/steps/imgSteps.jpg";
